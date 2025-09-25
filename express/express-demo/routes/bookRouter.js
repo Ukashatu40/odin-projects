8094228f9dc6c177aabb6fc783afaca68f3baad5
Ukashatu40 <ukashatuabdullahi40@gmail.com>
2025-09-25 16:25:58 +0100

    updated routes

--- express/express-demo/routes/bookRouter.js
+++ express/express-demo/routes/bookRouter.js
@@ -1,11 +1,9 @@
 // routes/bookRouter.js
 const { Router } = require("express");
 const bookRouter = Router();
+const { getBookById } = require("../controllers/bookController");
 
 bookRouter.get("/", (req, res) => res.send("All books"));
-bookRouter.get("/:bookId", (req, res) => {
-  const { bookId } = req.params;
-  res.send(`Book ID: ${bookId}`);
-});
+bookRouter.get("/:bookId", getBookById);
 
 module.exports = bookRouter;
--- express/express-demo/routes/indexRouter.js
+++ express/express-demo/routes/indexRouter.js
@@ -2,7 +2,11 @@
 
 const { Router } = require("express");
 const indexRouter = Router();
+const {indexController} = require("../controllers/indexController");
 
-indexRouter.get("/", (req, res) => res.send("Hello, world!"));
+const {books, authors} = require("../db");
+
+  
+indexRouter.get("/", indexController);
 
 module.exports = indexRouter;
