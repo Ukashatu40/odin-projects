fdf78bc582e35cb0025f992ac186f807b50ebd9f
Ukashatu40 <ukashatuabdullahi40@gmail.com>
2025-11-11 20:04:44 +0100

    issue: tobe fixed later

--- blog-api-backend/middlewares/authToken.js
+++ blog-api-backend/middlewares/authToken.js
@@ -6,7 +6,7 @@ module.exports = (req, res, next) => {
     const token = authHeader.split(" ")[1];
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded;
+        req.token = decoded;
         next();
     } catch (error) {
         return res.status(401).json({ message: "Invalid or expired token" });
