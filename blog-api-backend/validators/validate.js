1fd0f71db220e3e09324c62047ea4b3437bb9c54
Ukashatu40 <ukashatuabdullahi40@gmail.com>
2025-11-12 21:25:51 +0100

    feat: added post creation and registration validation

---
+++ blog-api-backend/validators/validate.js
@@ -0,0 +1,24 @@
+const {body} = require('express-validator');
+
+const validateRegistration = [
+    body('name')
+        .notEmpty().withMessage('Name is required')
+        .isLength({min: 2}).withMessage('Name must be at least 2 characters long'),
+    body('email')
+        .notEmpty().withMessage('Email is required')
+        .isEmail().withMessage('Invalid email format'),
+    body('password')
+        .notEmpty().withMessage('Password is required')
+        .isLength({min: 6}).withMessage('Password must be at least 6 characters long'),
+];
+
+const validatePostCreation = [
+    body('title')
+        .notEmpty().withMessage('Title is required')
+        .isLength({min: 5}).withMessage('Title must be at least 5 characters long'),
+    body('content')
+        .notEmpty().withMessage('Content is required')
+        .isLength({min: 20}).withMessage('Content must be at least 20 characters long'),
+];
+
+module.exports = {validateRegistration, validatePostCreation};
