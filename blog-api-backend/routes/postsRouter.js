1bfd0897028bfd17951a74d76821358c255aa144
Ukashatu40 <ukashatuabdullahi40@gmail.com>
2025-11-13 19:21:05 +0100

    feat: added /:id/comments endpoint

--- blog-api-backend/routes/postsRouter.js
+++ blog-api-backend/routes/postsRouter.js
@@ -8,5 +8,7 @@ const postsController = require('../controllers/postsController');
 postsRouter.get("/", authToken, postsController.getAllPosts);
 postsRouter.post("/", authToken, validatePostCreation, postsMiddleware, postsController.createPost);
 postsRouter.get("/:id", authToken, postsController.getParticularPost);
+postsRouter.get("/:id/comments", authToken, postsController.getCommentsForPost);
+postsRouter.post("/:id/comments", authToken, postsController.addCommentToPost);
 
 module.exports = postsRouter;
