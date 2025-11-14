9e53c79d3a01524a7a4712ea008dcaa84cba28ca
Ukashatu40 <ukashatuabdullahi40@gmail.com>
2025-11-14 21:13:16 +0100

    feat: added middleware for comments endpoint

--- blog-api-backend/routes/postsRouter.js
+++ blog-api-backend/routes/postsRouter.js
@@ -2,6 +2,8 @@ const postsRouter = require('express').Router();
 const authToken = require('../middlewares/authToken');
 const postsMiddleware = require('../middlewares/postsMiddleware');
 const {validatePostCreation} = require('../validators/validate');
+const {validateComment} = require('../validators/validate');
+const {validateCommentMiddleware} = require('../middlewares/commentsMiddleware');
 
 const postsController = require('../controllers/postsController');
 
@@ -9,6 +11,6 @@ postsRouter.get("/", authToken, postsController.getAllPosts);
 postsRouter.post("/", authToken, validatePostCreation, postsMiddleware, postsController.createPost);
 postsRouter.get("/:id", authToken, postsController.getParticularPost);
 postsRouter.get("/:id/comments", authToken, postsController.getCommentsForPost);
-postsRouter.post("/:id/comments", authToken, postsController.addCommentToPost);
+postsRouter.post("/:id/comments", authToken, validateComment, validateCommentMiddleware, postsController.addCommentToPost);
 
 module.exports = postsRouter;
