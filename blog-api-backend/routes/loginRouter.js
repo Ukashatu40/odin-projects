da4447708862b39fa872b1f0c2fec06a298da289
Ukashatu40 <ukashatuabdullahi40@gmail.com>
2025-11-14 21:12:17 +0100

    fix: added middlware for request object validation

--- blog-api-backend/routes/loginRouter.js
+++ blog-api-backend/routes/loginRouter.js
@@ -1,7 +1,8 @@
 const loginRouter = require('express').Router();
+const {validateLogin} = require('../validators/validate');
+const loginMiddleware = require('../middlewares/loginMiddleware');
 
 const loginController = require('../controllers/loginController');
 
-loginRouter.post("/", loginController);
-
+loginRouter.post("/",validateLogin, loginMiddleware, loginController);
 module.exports = loginRouter;
