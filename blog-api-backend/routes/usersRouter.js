2cfe57cf0a0863ca365b3fdf9f1c8d77e127d652
Ukashatu40 <ukashatuabdullahi40@gmail.com>
2025-11-15 21:21:35 +0100

    feat: Added additional Endpoint for this route

--- blog-api-backend/routes/usersRouter.js
+++ blog-api-backend/routes/usersRouter.js
@@ -4,5 +4,9 @@ const usersController = require('../controllers/usersController');
 const authToken = require('../middlewares/authToken');
 
 usersRouter.get("/", authToken, usersController.getAllUsers);
-
+usersRouter.get("/:id", authToken, usersController.getUserById);
+usersRouter.delete("/:id", authToken, usersController.deleteUser);
+// usersRouter.put("/:id", authToken, usersController.updateUser);
+usersRouter.get("/:id/posts", authToken, usersController.getPostsByUser);
+usersRouter.get("/:id/posts/:postId", authToken, usersController.getPostByUserById);
 module.exports = usersRouter;
