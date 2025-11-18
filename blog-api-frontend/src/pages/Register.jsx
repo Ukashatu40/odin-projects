5e9f60709cd0867a2798277836d4d99baf9e0394
Ukashatu40 <ukashatuabdullahi40@gmail.com>
2025-11-18 20:57:44 +0100

    test

--- blog-api-frontend/src/pages/Register.jsx
+++ blog-api-frontend/src/pages/Register.jsx
@@ -1,7 +1,7 @@
 export default function Register() {
     return (
         <div>
-            <h1>Register Page</h1>
+            <h1 className="text-white">Register Page</h1>
         </div>
     );
 }
