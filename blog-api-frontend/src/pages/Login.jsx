0b2601825147757e0413a061c8734c3f2f8187ee
Ukashatu40 <ukashatuabdullahi40@gmail.com>
2025-11-19 20:33:52 +0100

    first version of Login

--- blog-api-frontend/src/pages/Login.jsx
+++ blog-api-frontend/src/pages/Login.jsx
@@ -1,5 +1,52 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { API_BASE_URL } from "../config";
+import { toast } from "react-toastify";
+
 export default function Login() {
-    return (
+  const [form, setForm] = useState({ email: "", password: "" });
+  const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [errors, setErrors] = useState([]);
+  const navigate = useNavigate();
+
+  const handleChange = (e) =>
+    setForm({ ...form, [e.target.name]: e.target.value });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setMessage("");
+    try {
+      const res = await fetch(`${API_BASE_URL}/login`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(form),
+      });
+      const data = await res.json();
+      if (res.ok && data.token) {
+        localStorage.setItem("token", data.token);
+        localStorage.setItem("user", JSON.stringify(data.user));
+
+        // redirect to home page
+        toast.success(data.message);
+        navigate("/");
+      } else {
+        if (data.errors) {
+          setErrors(data.errors);
+        } else {
+          toast.error(data.message);
+          setMessage(data.message || "Login failed");
+        }
+      }
+    } catch (err) {
+      console.error(err);
+      setMessage("Network error");
+    } finally {
+      setLoading(false);
+    }
+  };
+  return (
     <>
       {/*
         This example requires updating your template:
@@ -22,14 +69,25 @@ export default function Login() {
             className="mx-auto h-10 w-auto not-dark:hidden"
           />
           <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900 dark:text-white">
-            Sign in to your account
+            Login in to your account
           </h2>
         </div>
 
+        {message && <p className="mt-3 text-red-600">{message}</p>}
+
+        {errors.map((error) => (
+          <p className="mt-3 text-red-600" key={error.path}>
+            {error.msg}
+          </p>
+        ))}
+
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form action="#" method="POST" className="space-y-6">
+          <form onSubmit={handleSubmit} method="POST" className="space-y-6">
             <div>
-              <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900 dark:text-gray-100">
+              <label
+                htmlFor="email"
+                className="block text-sm/6 font-medium text-gray-900 dark:text-gray-100"
+              >
                 Email address
               </label>
               <div className="mt-2">
@@ -38,6 +96,7 @@ export default function Login() {
                   name="email"
                   type="email"
                   required
+                  onChange={handleChange}
                   autoComplete="email"
                   className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6 dark:bg-white/5 dark:text-white dark:outline-white/10 dark:placeholder:text-gray-500 dark:focus:outline-indigo-500"
                 />
@@ -46,7 +105,10 @@ export default function Login() {
 
             <div>
               <div className="flex items-center justify-between">
-                <label htmlFor="password" className="block text-sm/6 font-medium text-gray-900 dark:text-gray-100">
+                <label
+                  htmlFor="password"
+                  className="block text-sm/6 font-medium text-gray-900 dark:text-gray-100"
+                >
                   Password
                 </label>
                 <div className="text-sm">
@@ -64,6 +126,7 @@ export default function Login() {
                   name="password"
                   type="password"
                   required
+                  onChange={handleChange}
                   autoComplete="current-password"
                   className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6 dark:bg-white/5 dark:text-white dark:outline-white/10 dark:placeholder:text-gray-500 dark:focus:outline-indigo-500"
                 />
@@ -75,7 +138,7 @@ export default function Login() {
                 type="submit"
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:bg-indigo-500 dark:shadow-none dark:hover:bg-indigo-400 dark:focus-visible:outline-indigo-500"
               >
-                Sign in
+                {loading ? "Logging..." : "Log In"}
               </button>
             </div>
           </form>
@@ -92,5 +155,5 @@ export default function Login() {
         </div>
       </div>
     </>
-  )
+  );
 }
