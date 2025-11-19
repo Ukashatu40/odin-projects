df0736009f253a860a4ba54435cab62d09b28fe1
Ukashatu40 <ukashatuabdullahi40@gmail.com>
2025-11-19 20:33:07 +0100

    feat: include ToastContainer

--- blog-api-frontend/src/App.jsx
+++ blog-api-frontend/src/App.jsx
@@ -1,6 +1,10 @@
 import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import HomePage from './pages/HomePage'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './App.css'
 
 function App() {
@@ -8,6 +12,7 @@ function App() {
 
   return (
     <Router>
+      <ToastContainer />
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
