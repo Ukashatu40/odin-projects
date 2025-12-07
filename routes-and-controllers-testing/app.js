0c6e553951923146674e7cce6508c1d4338e2111
Ukashatu40 <ukashatuabdullahi40@gmail.com>
2025-12-07 16:30:57 +0100

    Initial testing server

---
+++ routes-and-controllers-testing/app.js
@@ -0,0 +1,15 @@
+//// app.js
+const express = require("express");
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+
+const indexRouter = require("./index");
+app.use("/", indexRouter);
+
+app.listen(3000, (error) => {
+  if (error) {
+    throw error;
+  }
+  console.log("running");
+});
