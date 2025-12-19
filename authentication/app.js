920fbaeff86cb397ad4f606e493ef5b9844da892
Ukashatu Abdullahi <121878492+Ukashatu40@users.noreply.github.com>
2025-12-19 23:03:41 +0100

    Update app.js

--- authentication/app.js
+++ authentication/app.js
@@ -64,6 +64,7 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+// Useing Session
 app.use(session({
   store: sessionStore,
    secret: "cats", 
