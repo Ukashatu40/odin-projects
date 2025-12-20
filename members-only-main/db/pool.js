2965121ed5df5c136eafa1d7cf59ff0205c76d9f
Ukashatu Abdullahi <121878492+Ukashatu40@users.noreply.github.com>
2025-12-20 23:14:15 +0100

    Update pool.js for the better

--- members-only-main/db/pool.js
+++ members-only-main/db/pool.js
@@ -2,5 +2,6 @@ const { Pool } = require('pg')
 require('dotenv').config()
 
 module.exports = new Pool({
-    // Connection String below    connectionString: process.env.POSTGRES_URI_PROD
+    // Connection String below    
+    connectionString: process.env.POSTGRES_URI_PROD
 })
